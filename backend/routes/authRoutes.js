b7b13b679cb3baa577402172cf20b10d12eef892
azizunnabi <aziz484u@gmail.com>
2023-08-30 20:22:34 -0700

    ss
    sss

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const { register, login, changePassword, checkToken } = require("../controllers/authController");
+const { registerValidations, loginValidations, passwordValidations } = require("../validations/expressValidator");
+
+const router = express.Router();
+
+router.post("/register",registerValidations, register);
+router.post("/login", loginValidations, login);
+router.get("/check-token", checkToken);
+router.post("/changepassword",passwordValidations, changePassword);
+
+
+
+module.exports = router;
